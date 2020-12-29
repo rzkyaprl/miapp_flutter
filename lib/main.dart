30ab929f124bd335fe12dcca2ef2b75f0a0c8181
Radicorp <unduhdrive@gmail.com>
2020-12-29 20:11:53 +0800

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,67 @@ class Myapp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
-                  radius: 60.0,
-                  backgroundImage: AssetImage('images/photo.jpg')),
+                radius: 50.0,
+                backgroundImage: AssetImage('images/photo.jpg'),
+              ),
               Text(
                 'Rizky Apriliandi',
                 style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              )
+                    fontFamily: 'pacifico',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.white),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text('0812 3456 7891',
+                      style: TextStyle(
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20,
+                          color: Colors.teal,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text('rizky@kodinganku.com',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
             ],
           ),
         ),
